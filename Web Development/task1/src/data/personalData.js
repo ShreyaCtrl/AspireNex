f1f89ac672bd732583fd76c0687590c60e256862
Shreya Parkar <shreya.parkar.197@gmail.com>
2024-06-28 23:41:40 +0530

    Minute textual details modified

--- Web Development/task1/src/data/personalData.js
+++ Web Development/task1/src/data/personalData.js
@@ -5,7 +5,7 @@ export const personalData = {
   profile: profile,
   designation: "Software Developer",
   description:
-    "As an IT student, I am enthusiastic about learning from experts and eager to develop innovative solutions to existing problems. I love to learn and explore new technologies and am passionate about using my skills to develop something new new while learning. My passion for new technologies make me valuable asset which can fit into new teams. My eagerness to learn and drive for excellence make me a valuable asset to any team.",
+    "As an IT student, I am enthusiastic about learning from experts and eager to develop innovative solutions to existing problems. I love to learn and explore new technologies and am passionate about using my skills to develop something new while learning. My passion for new technologies make me valuable asset which can fit into new teams. My eagerness to learn and drive for excellence make me a valuable asset to any team.",
   email: "shreya.parkar.197@gmail.com",
   // phone: "+8801608797655",
   address: "Mumbai, India",
