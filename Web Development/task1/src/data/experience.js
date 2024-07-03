1e0eb3c48a68aa7c49ad79d4dbd597c1af9f27c3
Shreya Parkar <shreya.parkar.197@gmail.com>
2024-07-03 20:15:54 +0530

    Adding new data

--- Web Development/task1/src/data/experience.js
+++ Web Development/task1/src/data/experience.js
@@ -1,16 +1,16 @@
 export const experiences = [
   {
     id: 1,
+    title: "Web Developer",
+    company: "WebStack Academy",
+    duration: "(Jul 2025 - Present)"
+  },
+  {
+    id: 2,
     title: 'Full Stack Developer Intern',
     company: "Meraki Designing Services",
     duration: "(June 2025 - July 2025)"
   },
-  // {
-  //   id: 2,
-  //   title: "FullStack Developer",
-  //   company: "Fiverr (freelance)",
-  //   duration: "(Jun 2021 - Jan 2022)"
-  // },
   // {
   //   id: 3,
   //   title: "Self Employed",
